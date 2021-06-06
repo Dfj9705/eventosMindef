8091dbddcf76cf31ae48fc30a5d6cd35698b0be9
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-06-06 13:06:26 -0600

    Se muestran eventos en la vista home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Evento;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $eventos = Evento::where('fecha', '>', date('Y-m-d H:i'))
+        ->get();
+        // return view('eventos.index',compact('eventos'));
+        return view('home',compact('eventos'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,26 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12">
+            <h1>Próximos Eventos</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        @foreach ($eventos as $evento)
+
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <img class="card-img-top" src="/storage/{{ $evento->imagen }}" alt="Card image cap">
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">{{ $evento->nombre }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $evento->fecha }}</h6>
+                    <p class="card-text">{{ Str::limit($evento->descripcion, 35, '...') }}</p>
+                    <a href="{{ route('eventos.show', [ 'evento' => $evento->id ]) }}" class="card-link">Ver evento</a>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
