08001278bd67097db9fd2da50e3d716af97e3654
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-08-04 09:25:31 -0600

    Se colocolo https en lugar de http

--- resources/js/components/Code.js
+++ resources/js/components/Code.js
@@ -7,7 +7,7 @@ function Code( props) {
     let host = location.host
 
     return (
-        <QRCode value = { `http://${host}/ingreso/${props.token}` } />
+        <QRCode value = { `https://${host}/ingreso/${props.token}` } />
     );
 }
 
