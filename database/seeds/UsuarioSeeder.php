3aee6e079fe20056db1363bcfd03d5b7996a9236
Daniel Fuentes <abnerfuentes05@gmail.com>
2022-05-06 07:42:03 -0600

    Se agrego rol por defecto

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UsuarioSeeder extends Seeder
 {
@@ -19,6 +20,12 @@ class UsuarioSeeder extends Seeder
             'password' => Hash::make('admin123'),
             'email_verified_at' => date('Y-m-d H:i:s'),
         ]);
+
+        $rol = Role::create([
+            'name' => 'Administrador',
+        ]);
+
+        $user->assignRole($rol);
 
     }
 }
