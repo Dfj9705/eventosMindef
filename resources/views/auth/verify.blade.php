d8c4a30fdad3e37c3c6a4cd9ac1a56d7ba04b2c7
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2022-06-17 07:17:52 -0600

    Se agrego alerta de correo enviado a la vista de verificacion

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header lead text-center bg-dark text-white uppercase">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body text-center">
+                    <p class="h5">Revisa la bandeja de entrada de: <span class="font-weight-bold text-primary">{{ Auth::user()->email }}</span></p>
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
@@ -18,8 +19,9 @@
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link btn-block p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link btn-block p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
                     </form>
+                    
                 </div>
             </div>
         </div>
