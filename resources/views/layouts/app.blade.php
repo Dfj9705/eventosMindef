2e50fa7a8d2a7cb7b0bcdd0a9f6ce8531179b2ce
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2024-06-14 08:18:17 -0600

    CONFIGURACION DE PWA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('images/emdn.png') }}" type="image/x-icon">
+    @laravelPWA
 </head>
 <body>
     <div id="app">
